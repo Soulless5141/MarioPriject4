d30544e19498e8f00347bea3da1af32b7ac2484c
Soulless5141 <138739109+Soulless5141@users.noreply.github.com>
2025-02-05 11:48:07 +0900

    ItemBase軽く作成

--- Develop/Objects/Item/Kinoko.cpp
+++ Develop/Objects/Item/Kinoko.cpp
@@ -1,1 +1,44 @@
 #include "Kinoko.h"
+
+Kinoko::Kinoko()
+{
+}
+
+Kinoko::~Kinoko()
+{
+}
+
+void Kinoko::Initialize()
+{
+	// アニメーション画像の読み込み
+	ResourceManager* rm = ResourceManager::Get();
+
+	kinoko_animation = rm->GetImages("Resource/Images/Item/mushroom.png", 1, 1, 1, 32, 32);
+
+	__super::Initialize();
+}
+
+void Kinoko::Update(float delta_second)
+{
+}
+
+void Kinoko::Draw(const Vector2D& screen_offset) const
+{
+	__super::Draw(screen_offset);
+}
+
+void Kinoko::Finalize()
+{
+}
+
+void Kinoko::OnHitCollision(GameObject* hit_object)
+{
+}
+
+void Kinoko::Movement(float delta_second)
+{
+}
+
+void Kinoko::AnimationControl(float delta_second)
+{
+}
--- Develop/Objects/Item/Kinoko.h
+++ Develop/Objects/Item/Kinoko.h
@@ -1,5 +1,37 @@
 #pragma once
-class Kinoko
+#include "ItemBase.h"
+
+
+class Kinoko : public ItemBase
 {
+public:
+	std::vector<int> kinoko_animation;
+
+	Kinoko();
+	virtual ~Kinoko();
+
+	virtual void Initialize() override;
+	virtual void Update(float delta_second) override;
+	virtual void Draw(const Vector2D& screen_offset) const override;
+	virtual void Finalize() override;
+
+	/// <summary>
+	/// 当たり判定通知処理
+	/// </summary>
+	/// <param name="hit_object">当たったゲームオブジェクトのポインタ</param>
+	virtual void OnHitCollision(GameObject* hit_object) override;
+
+
+private:
+	/// <summary>
+	/// 移動処理
+	/// </summary>
+	/// <param name="delta_second">1フレームあたりの時間</param>
+	void Movement(float delta_second);
+	/// <summary>
+	/// アニメーション制御
+	/// </summary>
+	/// <param name="delta_second">1フレームあたりの時間</param>
+	void AnimationControl(float delta_second);
 };
 
